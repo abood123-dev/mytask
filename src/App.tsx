bcfebe32ba46efd773e3445ccfcc8fb5d0043344
Abood <abood@gmail.com>
2025-05-26 04:13:21 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { CSSProperties } from 'react';
 import './App.css';
+import { Link, useFetcher } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header >
+
+     </header>
+     </>
   );
 }
 
