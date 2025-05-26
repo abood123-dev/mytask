7df77e3fae930529e7ddb310cbc86abd16464f6f
Abood <abood@gmail.com>
2025-05-26 10:14:32 -0700

    Finishing the main page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './index.css';
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='border-box'>
+    <div className='mobile-container'>
+        <div className='welcome'>Welcome to PopX</div>
+        <div className='lorem'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</div>
+        <div><button className='btn1'> Create Account </button></div>
+        <div > <button className='btn2'>Already Registered? Login</button></div>
+    </div>
     </div>
   )
 }
