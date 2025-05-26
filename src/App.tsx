e3b029e76ea890d1f54db606832e526eb0206b7e
Abood <abood@gmail.com>
2025-05-26 06:18:23 -0700

    Beginning

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { CSSProperties } from 'react';
-import './App.css';
 import { Link, useFetcher } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+import './index.css';
 function App() {
 
   return (
     <>
-    <header >
-
-     </header>
+     <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <header className="text-red">
+        Tailwind is working!
+      </header>
+    </div>
      </>
   );
 }
